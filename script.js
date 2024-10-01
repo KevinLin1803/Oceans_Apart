2f4bef07143b8b92ffd0763f91d85df39913f176
Kevin Lin <kevinlin@Kevins-MacBook-Air.local>
2024-10-01 23:00:19 +0100

    Created the html structure for the to do list. Now just using javascript to add functionality. Will implement CSS at the end

---
+++ script.js
@@ -0,0 +1,16 @@
+function addTask() {
+
+    document.getElementById("task-1").innerText = "hello"
+    
+    // Not sure if this first line works either
+
+    // let task_name = document.getElementsById("task-input").value
+    // let task_list = document.getElementsById("tasks")
+
+    // let newtask_item = document.createElement("li")
+    // new_task_item.textContent = `${task_name}`
+
+    // task_list.appendChild(newtask_item)
+}
+
+export default addTask
