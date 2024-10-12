1a12ef4da361e5829928cfbc035302a1161d89ce
Kevin Lin <kevinlin@Kevins-MacBook-Air.local>
2024-10-13 01:41:28 +0200

    Stylesheet and icons added back

--- front-end/src/app.js
+++ front-end/src/app.js
@@ -6,9 +6,12 @@ const fs = require('fs')
 var bodyParser = require('body-parser')
 var jsonParser = bodyParser.json()
 
-
 const app = express()
 const port = 5500
+
+app.use(express.static(__dirname))
+app.use(express.static(__dirname + '/icons'))
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'))
@@ -19,7 +22,6 @@ app.get('/api/users', (req, res) => {
 })
 
 app.post('/api/users', jsonParser, (req, res) => {
-    // I think I need to use req here?
     let new_tasks = req.body    
     data.push(new_tasks)
     // You need __dirname because that's the current directory
@@ -30,6 +32,11 @@ app.listen(port, () => {
     console.log(`Server is listening on port ${port}`)
 })
 
+// I want to connect it 
+// Perfect the add
+// And then I can work on the other requests
+// Then we'll have something kinda cool to work with --> sus out tutorials/contacting other peoeple as well :)
+// Work on being as smart and efficient with your applicatino as possible
 
 // Tomorrow's tasks --> get a functioning backend to work (create, delete, edit as well)
 
--- front-end/src/script.js
+++ front-end/src/script.js
@@ -1,16 +1,93 @@
-// function addTask() {
+function addTask(gender) {
+    // Prevent default here is preventing it from actually submitting
+    event.preventDefault();
 
-//     document.getElementById("task-1").innerText = "hello"
+    // Javascript equality
+    if (gender == 'boy') {
+        var tasks = 'tasks-boy'
+        var task_input = 'task-input-boy'
+    } else {
+        var tasks = 'tasks-girl'
+        var task_input = 'task-input-girl'
+    }
+
+    let id = document.querySelectorAll(`#${tasks} div`).length
+
+    let task_name = document.getElementById(`${task_input}`).value
+
+    if (task_name == '') {
+        return
+    }
+
+    displayTask(gender, id, task_name, tasks)
+
+    // Resetting the input bar
+    document.getElementById(`${task_input}`).value = ''
+
+    // Why do we need this header?
+    try {
+        const response = fetch('/api/users', {
+        method: "POST",
+        body: JSON.stringify({task: task_name, id: id, gender}),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+function displayTask(gender, id, task_name, tasks){
+    let new_task = document.createElement("div")
+    new_task.innerHTML = `<input class = "task" type="checkbox" onclick = "completeTask('${gender}')" id="${gender}-${id}""> </input>
+                          <label for = "${gender}-${id}" contenteditable="true"> ${task_name} </label>
+                          <button class = "delete-task-button" onclick = "removeTask('${gender}-${id}')" > <img src="../icons/trash-can.png" id="trash-icon"> </button>`
+
+    new_task.className = "task-container-single"
+
+    document.getElementById(`${tasks}`).appendChild(new_task)
+}
+
+function completeTask(gender) {
+    if (gender == 'boy') {
+        var tasks = 'tasks-boy'
+        var completed_tasks = 'completed-tasks-boy'
+    } else {
+        var tasks = 'tasks-girl'
+        var completed_tasks = 'completed-tasks-girl'
+    }
+
+    let inputs = document.querySelectorAll(`#${tasks} div`)
     
-//     // Not sure if this first line works either
+    for (let i = 0; i < inputs.length; i++) {
+        // did the turnerary force shut it down before?
+        if (inputs[i].querySelector("input").checked) {
+            let result = document.getElementById(`${completed_tasks}`).appendChild(inputs[i])
+        }
+    }
+}
 
-//     // let task_name = document.getElementsById("task-input").value
-//     // let task_list = document.getElementsById("tasks")
+function removeTask(id) {
+    let parent = document.getElementById(id).parentNode
+    parent.remove()
+}
 
-//     // let newtask_item = document.createElement("li")
-//     // new_task_item.textContent = `${task_name}`
+//Updating the date
+document.getElementById("date").innerHTML = Date().substring(0,15)
 
-//     // task_list.appendChild(newtask_item)
-// }
-
-// export default addTask
+try {
+    fetch('/api/users')
+        .then(response => response.json())
+        .then(task_data => array.forEach(task => {
+            if (task.gender == 'boy') {
+                var tasks = 'tasks-boy'
+            } else {
+                var tasks = 'tasks-girl'
+            }
+            displayTask(task.gender, task.id, task.task, tasks)
+            console.log('added', task)
+        }))
+  } catch (error) {
+    console.error(error.message);
+}
