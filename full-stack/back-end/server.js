e5d7d2f3846de12d6fa1ae7bc56280f7d9f90f03
Kevin Lin <kevinlin@Kevins-MacBook-Air.local>
2024-11-25 13:33:25 +0000

    Updated the styling a little and configured more environment variables for deployment

--- full-stack/back-end/server.js
+++ full-stack/back-end/server.js
@@ -17,7 +17,7 @@ app.use(cors())
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-      origin: "http://localhost:3000",
+      origin: process.env.FRONTEND_URL,
       methods: ["GET", "POST", "DELETE", "PUT"]
     }
   });
