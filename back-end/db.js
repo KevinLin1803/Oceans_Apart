c93d1f72ff24e5ac7be0fb9890e8ad3871afa8f9
Kevin Lin <135843131+KevinLin1803@users.noreply.github.com>
2024-11-25 18:58:48 +0000

    Update db.js

--- back-end/db.js
+++ back-end/db.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 const pool = new Pool({
     host: process.env.HOST,
     user: process.env.USERNAME,
-    port: process.env.DB_PORT,
+    port: process.env.DBPORT,
     password: process.env.PASSWORD,
     database: "To-Do-App",
     max: 20,               // Reduce max connections to make debugging easier
